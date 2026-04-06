da40aa45a18fb72112ca2710f8cafcdba63f0bdd
wujingbang <wujingbang@gmail.com>
2026-04-06 13:22:38 +0800

    Part2:一些FIFO等IP核数据的修改文件。注意为了修BUG改了一些FIFO的输入输出位宽，且深度也需要进一步的测试

--- ip_repo/axidma_middleware_1.2/xgui/axidma_middleware_v1_2_v1_2.tcl
+++ ip_repo/axidma_middleware_1.2/xgui/axidma_middleware_v1_2_v1_2.tcl
@@ -3,22 +3,14 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  ipgui::add_param $IPINST -name "C_LENGTH_WIDTH" -parent ${Page_0}
   ipgui::add_param $IPINST -name "C_M_AXI_ADDR_WIDTH" -parent ${Page_0}
   ipgui::add_param $IPINST -name "C_M_AXI_DATA_WIDTH" -parent ${Page_0}
   ipgui::add_param $IPINST -name "C_S_AXI_ADDR_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S_AXI_ARUSER_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S_AXI_AWUSER_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S_AXI_BUSER_WIDTH" -parent ${Page_0}
   ipgui::add_param $IPINST -name "C_S_AXI_DATA_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S_AXI_ID_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S_AXI_RUSER_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S_AXI_WUSER_WIDTH" -parent ${Page_0}
 
   ipgui::add_param $IPINST -name "CH0_DMA_MM2S_ADDR_BASE"
   ipgui::add_param $IPINST -name "CH0_DMA_S2MM_ADDR_BASE"
-  ipgui::add_param $IPINST -name "CH1_DMA_MM2S_ADDR_BASE"
-  ipgui::add_param $IPINST -name "CH1_DMA_S2MM_ADDR_BASE"
+  ipgui::add_param $IPINST -name "DEVICE_NUMBER"
 
 }
 
@@ -37,24 +29,6 @@ proc update_PARAM_VALUE.CH0_DMA_S2MM_ADDR_BASE { PARAM_VALUE.CH0_DMA_S2MM_ADDR_B
 
 proc validate_PARAM_VALUE.CH0_DMA_S2MM_ADDR_BASE { PARAM_VALUE.CH0_DMA_S2MM_ADDR_BASE } {
 	# Procedure called to validate CH0_DMA_S2MM_ADDR_BASE
-	return true
-}
-
-proc update_PARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE { PARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE } {
-	# Procedure called to update CH1_DMA_MM2S_ADDR_BASE when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE { PARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE } {
-	# Procedure called to validate CH1_DMA_MM2S_ADDR_BASE
-	return true
-}
-
-proc update_PARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE { PARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE } {
-	# Procedure called to update CH1_DMA_S2MM_ADDR_BASE when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE { PARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE } {
-	# Procedure called to validate CH1_DMA_S2MM_ADDR_BASE
 	return true
 }
 
@@ -157,6 +131,15 @@ proc validate_PARAM_VALUE.C_S_AXI_WUSER_WIDTH { PARAM_VALUE.C_S_AXI_WUSER_WIDTH 
 	return true
 }
 
+proc update_PARAM_VALUE.DEVICE_NUMBER { PARAM_VALUE.DEVICE_NUMBER } {
+	# Procedure called to update DEVICE_NUMBER when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DEVICE_NUMBER { PARAM_VALUE.DEVICE_NUMBER } {
+	# Procedure called to validate DEVICE_NUMBER
+	return true
+}
+
 
 proc update_MODELPARAM_VALUE.C_LENGTH_WIDTH { MODELPARAM_VALUE.C_LENGTH_WIDTH PARAM_VALUE.C_LENGTH_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -223,13 +206,8 @@ proc update_MODELPARAM_VALUE.CH0_DMA_S2MM_ADDR_BASE { MODELPARAM_VALUE.CH0_DMA_S
 	set_property value [get_property value ${PARAM_VALUE.CH0_DMA_S2MM_ADDR_BASE}] ${MODELPARAM_VALUE.CH0_DMA_S2MM_ADDR_BASE}
 }
 
-proc update_MODELPARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE { MODELPARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE PARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE } {
+proc update_MODELPARAM_VALUE.DEVICE_NUMBER { MODELPARAM_VALUE.DEVICE_NUMBER PARAM_VALUE.DEVICE_NUMBER } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE}] ${MODELPARAM_VALUE.CH1_DMA_MM2S_ADDR_BASE}
-}
-
-proc update_MODELPARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE { MODELPARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE PARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE}] ${MODELPARAM_VALUE.CH1_DMA_S2MM_ADDR_BASE}
+	set_property value [get_property value ${PARAM_VALUE.DEVICE_NUMBER}] ${MODELPARAM_VALUE.DEVICE_NUMBER}
 }
 
